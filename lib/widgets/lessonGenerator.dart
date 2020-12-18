b8fa1810fe2ed433bedcce163ee0ac63de0086fe
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-12-18 17:21:10 +0000

    Added colour picker

--- lib/widgets/lessonGenerator.dart
+++ lib/widgets/lessonGenerator.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 // widgets
 import './newLessonDialog.dart';
 
-class LessonGenerator extends StatelessWidget {
+class LessonGenerator extends StatefulWidget {
   final Map<String, dynamic> lessons;
   final Widget pageNavigationButtons;
 
   LessonGenerator({Key key, this.lessons, this.pageNavigationButtons})
       : super(key: key);
+
+  @override
+  _LessonGeneratorState createState() => _LessonGeneratorState();
+}
+
+class _LessonGeneratorState extends State<LessonGenerator> {
+  Color currentColour =
+      Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
+
+  void changeColour(Color color) => setState(() {
+        print(color);
+        currentColour = color;
+      });
 
   void addPeriod(data) {}
 
@@ -44,14 +59,14 @@ class LessonGenerator extends StatelessWidget {
                 onPressed: () => addLesson(context),
               ),
             ),
-            pageNavigationButtons,
+            widget.pageNavigationButtons,
           ],
         ),
       ),
     );
   }
 
-  addLesson(BuildContext context) {
+  addLesson(BuildContext context, Color colour, Function changeColour) {
     return showModalBottomSheet(
       isScrollControlled: true,
       shape: RoundedRectangleBorder(
@@ -67,72 +82,130 @@ class LessonGenerator extends StatelessWidget {
                 right: 20,
                 left: 20,
                 bottom: MediaQuery.of(context).viewInsets.bottom + 20),
-            child: Form(
-              // key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.1,
-                    height: 5,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(5),
-                        color: Theme.of(context).splashColor),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      "Add Lesson",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  width: 30,
+                  height: 5,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(5),
+                      color: Theme.of(context).splashColor),
+                ),
+                Form(
+                  // key: _formKey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            "Add Lesson",
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.class_),
+                            labelText: "Name",
+                            border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(width: 0, style: BorderStyle.none),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 10),
+                        GestureDetector(
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  titlePadding: const EdgeInsets.all(0.0),
+                                  contentPadding: const EdgeInsets.all(0.0),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25.0),
+                                  ),
+                                  content: SingleChildScrollView(
+                                    child: MaterialPicker(
+                                      pickerColor: colour,
+                                      onColorChanged: changeColour,
+                                      enableLabel: true,
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                          child: Container(
+                            height: 50,
+                            padding: EdgeInsets.only(left: 10.0),
+                            alignment: Alignment.centerLeft,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: colour,
+                            ),
+                            child: Text(
+                              "Colour",
+                              style: TextStyle(
+                                color: useWhiteForeground(currentColour)
+                                    ? const Color(0xffffffff)
+                                    : const Color(0xff000000),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.school),
+                            labelText: "Teacher",
+                            border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(width: 0, style: BorderStyle.none),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.place),
+                            labelText: "Room",
+                            border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(width: 0, style: BorderStyle.none),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        RaisedButton(child: Text("Add"), onPressed: () {})
+                      ],
                     ),
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.class_),
-                      labelText: "Name",
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.school),
-                      labelText: "Teacher",
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.place),
-                      labelText: "Room",
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  RaisedButton(
-                    child: Text("Add"),
-                    onPressed: () {},
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         );
