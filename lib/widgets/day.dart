122483c2eb25805fc12505c9484277e63a835890
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2020-12-27 16:43:23 +0000

    Periods now display on timetable

--- lib/widgets/day.dart
+++ lib/widgets/day.dart
@@ -2,20 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:intl/intl.dart';
 
-class Period extends StatelessWidget {
+// Widgets
+import './expandedSelection.dart';
+
+class Period extends StatefulWidget {
   final lesson;
   final period;
 
   Period({Key key, this.lesson, this.period}) : super(key: key);
+  @override
+  _PeriodState createState() => _PeriodState();
+}
 
+class _PeriodState extends State<Period> {
   final DateFormat formatter = DateFormat.Hm();
+  bool expanded = false;
 
   @override
   Widget build(BuildContext context) {
     Color backgroundColour = Color.fromRGBO(
-      lesson["colour"]["red"],
-      lesson["colour"]["green"],
-      lesson["colour"]["blue"],
+      widget.lesson["colour"]["red"],
+      widget.lesson["colour"]["green"],
+      widget.lesson["colour"]["blue"],
       1,
     );
 
@@ -24,30 +32,101 @@ class Period extends StatelessWidget {
         : const Color(0xff000000);
 
     return Card(
-      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      margin: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
       color: backgroundColour,
-      child: ExpansionTile(
-        title: Row(
-          children: [
-            Text(
-              formatter.format(DateTime.parse(period["start"])),
-              style: TextStyle(color: textColour),
-            ),
-            Text(
-              lesson["name"],
-              style: TextStyle(color: textColour, fontWeight: FontWeight.w900),
-            ),
-          ],
-        ),
-        children: [
-          Text(
-            "|",
-            style: TextStyle(color: textColour),
+      child: InkWell(
+        onTap: () => setState(() => expanded = !expanded),
+        splashColor: backgroundColour,
+        borderRadius: BorderRadius.circular(10),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 70,
+                    alignment: Alignment.center,
+                    child: Text(
+                      formatter.format(DateTime.parse(widget.period["start"])),
+                      style: TextStyle(
+                          color: textColour,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w900),
+                    ),
+                  ),
+                  Container(
+                    constraints: BoxConstraints(maxWidth: 10),
+                  ),
+                  Text(
+                    widget.lesson["name"],
+                    style: TextStyle(
+                      color: textColour,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+              ExpandedSection(
+                expand: expanded,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 70,
+                      alignment: Alignment.center,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "to",
+                            style: TextStyle(color: textColour, height: 1),
+                          ),
+                          Text(
+                            formatter
+                                .format(DateTime.parse(widget.period["end"])),
+                            style: TextStyle(
+                                color: textColour,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w900),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      constraints: BoxConstraints(maxWidth: 10),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (widget.lesson["teacher"] != "")
+                          Text(
+                            "Teacher: " + widget.lesson["teacher"],
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 12,
+                              color: textColour.withAlpha(155),
+                            ),
+                          ),
+                        if (widget.lesson["room"] != "")
+                          Text(
+                            "Room: " + widget.lesson["room"],
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 12,
+                              color: textColour.withAlpha(155),
+                            ),
+                          ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
