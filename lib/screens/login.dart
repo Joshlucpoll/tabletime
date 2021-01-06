679a0c53c798f41d817d3004e4f65bb526812273
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2021-01-06 01:29:36 +0000

    Added Google sign in to app

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -39,7 +39,7 @@ class _LoginState extends State<Login> {
               children: <Widget>[
                 Image(
                   image: AssetImage("assets/images/tabletime_logo.png"),
-                  width: MediaQuery.of(context).size.shortestSide * 0.3,
+                  width: MediaQuery.of(context).size.shortestSide * 0.2,
                 ),
                 Container(
                   constraints: BoxConstraints(maxWidth: 500),
@@ -84,44 +84,72 @@ class _LoginState extends State<Login> {
                     SizedBox(height: 20.0),
                     RaisedButton(
                       key: const ValueKey("signIn"),
-                      onPressed: () async {
-                        final String retVal = await widget._auth.signIn(
-                          email: _emailController.text,
-                          password: _passwordController.text,
-                        );
-                        if (retVal != "Success") {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(retVal),
+                      onPressed: () => widget._auth
+                          .signIn(
+                            email: _emailController.text,
+                            password: _passwordController.text,
+                          )
+                          .catchError(
+                            (e) => ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(e.message),
+                              ),
                             ),
-                          );
-                        }
-                      },
+                          ),
                       child: const Text("Sign In"),
                     ),
                     SizedBox(height: 5.0),
                     FlatButton(
                       key: const ValueKey("createAccount"),
-                      onPressed: () async {
-                        final String retVal = await widget._auth.createAccount(
-                          email: _emailController.text,
-                          password: _passwordController.text,
-                        );
-                        if (retVal == "Success") {
-                          _emailController.clear();
-                          _passwordController.clear();
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(retVal),
+                      onPressed: () => widget._auth
+                          .createAccount(
+                            email: _emailController.text,
+                            password: _passwordController.text,
+                          )
+                          .catchError(
+                            (e) => ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(e.message),
+                              ),
                             ),
-                          );
-                        }
-                      },
+                          ),
                       child: Text(
                         "Create Account",
                       ),
-                    )
+                    ),
+                    RaisedButton(
+                      padding: EdgeInsets.all(0),
+                      onPressed: () =>
+                          widget._auth.signInWithGoogle().catchError(
+                                (e) =>
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(e.message),
+                                  ),
+                                ),
+                              ),
+                      color: Color(0xFF3469C1),
+                      textColor: Colors.white,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            margin: EdgeInsets.only(right: 10),
+                            color: Colors.white,
+                            child: Image(
+                              image:
+                                  AssetImage("assets/images/google_logo.png"),
+                              height: 18,
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Text("Sign in with Google"),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ],
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:get_it/get_it.dart';
 import './database.dart';
 
@@ -9,33 +10,41 @@ class Auth {
 
   String get uid => auth.currentUser.uid;
 
-  Future<String> createAccount({String email, String password}) async {
-    try {
-      await auth.createUserWithEmailAndPassword(
-        email: email.trim(),
-        password: password.trim(),
-      );
+  Future<UserCredential> createAccount({String email, String password}) async {
+    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+      email: email.trim(),
+      password: password.trim(),
+    );
+
+    if (userCredential.additionalUserInfo.isNewUser) {
       await GetIt.I.get<Database>().addUser();
-      return "Success";
-    } on FirebaseAuthException catch (e) {
-      return e.message;
-    } catch (e) {
-      rethrow;
     }
+    return userCredential;
   }
 
-  Future<String> signIn({String email, String password}) async {
-    try {
-      await auth.signInWithEmailAndPassword(
-        email: email.trim(),
-        password: password.trim(),
-      );
-      return "Success";
-    } on FirebaseAuthException catch (e) {
-      return e.message;
-    } catch (e) {
-      rethrow;
+  Future<UserCredential> signIn({String email, String password}) async {
+    return await auth.signInWithEmailAndPassword(
+      email: email.trim(),
+      password: password.trim(),
+    );
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    UserCredential userCredential = await auth.signInWithCredential(credential);
+    if (userCredential.additionalUserInfo.isNewUser) {
+      await GetIt.I.get<Database>().addUser();
     }
+    return userCredential;
   }
 
   Future<String> signOut() async {
